4552e2a07296fcbfaf7999312ba00fec6d69f10b
Evan Garrison <evangarr7@gmail.com>
2021-05-28 20:50:33 -0700

    Added comments to driver file: mini_shrec_bot.ino

--- mini_shrec_bot.ino
+++ mini_shrec_bot.ino
@@ -1,30 +1,56 @@
-#include "drive_train.h"
+/*********************************************************************
+** Program Filename: mini_shrec_bot.ino
+** Author: Evan Garrison
+** Date: 05/28/2021
+** Description: main driver file for 'lil SHREC car.
+*********************************************************************/
 
-int
+#include "drive_train.h" //includes custom-made DriveTrain class
+
+int //pins used for motor controller
 leftA = 9,
 leftB = 10,
 rightA = 11,
 rightB = 12;
 
-DriveTrain drive(9, 10, 11, 12);
+/**
+ * Drivetrain declaration and definition.
+ * Creates a new instance of DriveTrain called drive,
+ * with pins leftA, leftB, rightA, rightB
+ */
+DriveTrain drive(leftA, leftB, rightA, rightB);
 
 
 void setup()
 {
-    Serial.begin(9600);
+    //Serial.begin(9600);
 }
+
+/**
+ * To use the drivetrain created above, use the following format in loop():
+ * to set a direction: drive.setDirection(left, right);
+ * Where left and right are replaced with DriveTrain::FORWARD,
+ * DriveTrain::REVERSE, or DriveTrain::NEUTRAL
+ * Notice you have to preface the direction with DriveTrain::
+ * This is because the custom type used for directions is declared and defined
+ * in the DriveTrain class.
+ */
 
 void loop()
 {
+    //sets motors to forward for 2 seconds
 	drive.setDirections(DriveTrain::FORWARD, DriveTrain::FORWARD);
     delay(2000);
 
+    //sets motors to forward and reverse for 2 seconds, should make bot turn
     drive.setDirections(DriveTrain::FORWARD, DriveTrain::REVERSE);
     delay(2000);
 
+    //sets motors to reverse for 2 seconds
     drive.setDirections(DriveTrain::REVERSE, DriveTrain::REVERSE);
     delay(2000);
 
+    //stops motors for 2 seconds
     drive.setDirections(DriveTrain::NEUTRAL, DriveTrain::NEUTRAL);
     delay(2000);
 }
